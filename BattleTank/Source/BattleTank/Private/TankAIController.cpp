2edc112b5b114da75767ff12d9cf6fd9ed7d34f1
CHRISDESKTOP\Owner <chris.sawatzky@hotmail.com>
2018-09-20 16:17:30 -0600

    the AI tanks are finding the player controlled tank

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankAIController.h"
+#include "Engine/World.h"
 
+//get a reference to itself (AI Tank), or rather the possessed tank
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
@@ -10,14 +12,23 @@ ATank* ATankAIController::GetControlledTank() const
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	auto AITank = GetControlledTank();
-	if (!AITank)
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Tank Not Possessed by AI Controller"))
+		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank"))
 	}
 	else
 	{
-		UE_LOG(LogTemp,Warning, TEXT("%s possesed by AI Controller"), *(AITank->GetName()))
+		UE_LOG(LogTemp,Warning, TEXT("AIController found player: %s"), *(PlayerTank->GetName()))
 	}
 	
+}
+
+//gets a reference to the player tank (you)
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+
+	return Cast<ATank>(PlayerPawn);
 }
